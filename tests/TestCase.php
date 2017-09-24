0720c3efb2f1c05a460aa5c36ad13aaa2c19ea44
VictorAvelar <aovo128446@upemor.edu.mx>
2017-09-24 23:56:51 +0200

    Add Adam Whatan hat tip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,69 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Test setup.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->withExceptionHandling();
+    }
+
+    /**
+     * Testing login functionality.
+     *
+     * @param null $user
+     * @return $this
+     */
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create(User::class);
+
+        $this->actingAs($user);
+
+        return $this;
+    }
+
+    /**
+     * Enhances the information provided by the test response.
+     * by @adamwhatan.
+     */
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+
+            public function __construct()
+            {
+            }
+
+            public function report(\Exception $e)
+            {
+            }
+
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
+
+    /**
+     * Disables the exception handling.
+     * @return $this
+     */
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+        return $this;
+    }
 }
